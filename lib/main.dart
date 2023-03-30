0a8410b24d0194ad43fad1ab722d6fcbe4473dfc
Habibieproject <Habibieproject>
2023-03-30 23:38:25 +0700

    tes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:belajar_flutter/view/home.dart';
+import 'package:belajar_flutter/view/halaman%202.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,20 +12,19 @@ class Aplikasiku extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: "Belajar"),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyWidget());
   }
 }
--- lib/view/halaman 2.dart
+++ lib/view/halaman 2.dart
@@ -1,95 +1,84 @@
 import 'package:flutter/material.dart';
 
-class SecondPage extends StatefulWidget {
-  const SecondPage({super.key});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class MyWidget extends StatefulWidget {
+  const MyWidget({Key? key}) : super(key: key);
 
   @override
-  State<SecondPage> createState() => _SecondPageState();
+  _MyWidgetState createState() => _MyWidgetState();
 }
 
-class _SecondPageState extends State<SecondPage> {
-  int hitung = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // hitung without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      hitung++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // hitung without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      hitung--;
-    });
-  }
+class _MyWidgetState extends State<MyWidget> {
+  String _title = 'Expandable List';
+  bool _isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: <Widget>[
-        const Text(
-          'You have pushed the button this many times:',
-        ),
-        Text(
-          "$hitung",
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
-        const SizedBox(
-          height: 20,
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Widget'),
+      ),
+      body: ListView(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                "Header",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          newMethod(),
+        ],
+      ),
+    );
+  }
+
+  bool _isChecked = false;
+  ExpansionTile newMethod() {
+    return ExpansionTile(
+      title: CheckboxListTile(
+        title: const Text('Expandable List'),
+        value: _isChecked,
+        onChanged: (bool? value) {
+          setState(() {
+            _isChecked = value!;
+          });
+        },
+        controlAffinity: ListTileControlAffinity.leading,
+      ),
+      onExpansionChanged: (bool isExpanded) {
+        setState(() {
+          _isExpanded = isExpanded;
+          _title = isExpanded ? 'Expanded List' : 'Expandable List';
+        });
+      },
+      children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            GestureDetector(
-              onTap: () {
-                _decrementCounter();
-              },
-              child: const CircleAvatar(
-                child: Icon(Icons.minimize),
-              ),
-            ),
-            const SizedBox(
-              width: 20,
-            ),
-            GestureDetector(
-              onTap: () {
-                _incrementCounter();
-              },
-              child: const CircleAvatar(
-                child: Icon(Icons.add),
-              ),
+            DataTable(
+              columns: const [
+                DataColumn(label: Text('')),
+                DataColumn(label: Text('')),
+                DataColumn(label: Text('')),
+              ],
+              rows: const [
+                DataRow(cells: [
+                  DataCell(Text('Row 1')),
+                  DataCell(Text('Row 1,')),
+                  DataCell(Text('Row 1, ')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('Row 2,')),
+                  DataCell(Text('Row 2,')),
+                  DataCell(Text('Row 2, ')),
+                ]),
+              ],
             ),
           ],
-        )
+        ),
       ],
-    ));
+    );
   }
 }
